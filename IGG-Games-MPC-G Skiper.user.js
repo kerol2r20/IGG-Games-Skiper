499c21d5d35f68b7409a21c8037461320aa72f3a
Yu-Hsin <kerol2r20@gmail.com>
2016-05-19 17:45:41 +0800

    Add MPC-G Support

--- IGG-Games-MPC-G Skiper.user.js
+++ IGG-Games-MPC-G Skiper.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name         IGG-Games Skiper
+// @name         IGG-Games/MPC-G Skiper
 // @namespace    https://github.com/kerol2r20
 // @version      0.1
 // @description  Skip wating AD page in IGG games download link
 // @author       Yu-Hsin Lu
 // @include      http://igg-games.com/*
+// @include      http://www.mpc-g.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js
 // @grant       unsafeWindow
 // ==/UserScript==
 
 (function() {
+    'use strict';
     var pat = /.*xurl=(s?:.*)/i;
     $("a").each(function() {
         var url=$(this).attr('href');
@@ -19,5 +21,10 @@
         if(result!==null){
             $(this).attr('href','http'+result[1]);
         }
+    });
+    $("form[name*='download']").each(function() {
+        // $("this > input[name='downloadLink']").attr('value');
+        var url = $(this).children("input[name='downloadLink']").attr('value');
+        $(this).children("a").attr('href',url);
     });
 })();
