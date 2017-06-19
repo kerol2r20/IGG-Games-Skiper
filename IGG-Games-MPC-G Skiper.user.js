7aaefbad0f301a420b4778239668a1b9afe89f66
kerol2r20 <kerol2r20@gmail.com>
2017-06-20 04:58:15 +0800

    Complete collections

--- IGG-Games-MPC-G Skiper.user.js
+++ IGG-Games-MPC-G Skiper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IGG-Games/MPC-G Skiper
 // @namespace    https://greasyfork.org/zh-TW/scripts/19802-igg-games-mpc-g-skiper
-// @version      1.2
+// @version      1.2.1
 // @description  Skip wating AD page in IGG games download link
 // @author       Yu-Hsin Lu
 // @include      http://igg-games.com/*
@@ -18,9 +18,10 @@ const megapat = /mega\.nz/;
 (function() {
     let collections = {mega: [],google: []};
     let anchors = document.querySelectorAll('a');
+    let processes = [];
     anchors = _.filter(anchors, anchor => pat.test(anchor.href));
     anchors.forEach(anchor => {
-        fetch(anchor.href).then(res => res.text()).then(data => {
+        const process = fetch(anchor.href).then(res => res.text()).then(data => {
             const parser = document.createElement('html');
             parser.innerHTML = data;
             const targetDOM = parser.querySelector('meta[http-equiv="refresh"]');
@@ -29,7 +30,13 @@ const megapat = /mega\.nz/;
                 targetURL = decodeURIComponent(targetURL);
                 targetURL = `http${targetURL}`;
                 anchor.href=targetURL;
+                if(googlepat.test(targetURL)) collections.google.push(targetURL);
+                if(megapat.test(targetURL)) collections.mega.push(targetURL);
             }
         });
+        processes.push(process);
+    });
+    Promise.all(processes).then(() => {
+        console.log(collections);
     });
 })();
